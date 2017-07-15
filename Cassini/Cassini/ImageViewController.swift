d326c95871a0939df0062724aa195316a077228d
Kelvin <et.kelvin@gmail.com>
2017-07-15 23:30:58 +0800

    add s spinner for fetching images

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -19,7 +19,9 @@ class ImageViewController: UIViewController {
             scrollView.addSubview(imageView)
         }
     }
-
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     //  will be set by segue
     var imageURL: URL? {
         didSet {
@@ -33,6 +35,8 @@ class ImageViewController: UIViewController {
     
     private func fetchImage() {
         if let url = imageURL {
+            //  start spinning when fetching
+            spinner.startAnimating()
             //  Data() will throw error, need to "try"
             //  init(contentsOf: URL, options: Data.ReadingOptions)
             //  Creates a data buffer with the contents of a URL.
@@ -63,6 +67,7 @@ class ImageViewController: UIViewController {
             //  whenever imageView.image is set, rescale the contentSize
             //  use optional chaining, scrollView may not be available yet
             scrollView?.contentSize = imageView.frame.size
+            spinner?.stopAnimating()
         }
     }
     
