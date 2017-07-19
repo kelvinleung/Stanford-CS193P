90de2f0d8df7cfad48acff616f3d800d5bca2e3d
Kelvin <et.kelvin@gmail.com>
2017-07-20 00:04:44 +0800

    add the tweetersVC

---
+++ Smashtag/Smashtag/SmashTweetersTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SmashTweetersTableViewController.swift
+//  Smashtag
+//
+//  Created by Kelvin Leung on 19/07/2017.
+//  Copyright © 2017 Kelvin Leung. All rights reserved.
+//
+
+import UIKit
+
+class SmashTweetersTableViewController: UITableViewController {
+}
